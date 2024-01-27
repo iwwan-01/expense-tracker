16fd6918fe87935ff8aed7c2d9d569d7196ef405
iwwan-01 <89984902+iwwan-01@users.noreply.github.com>
2024-01-27 19:23:01 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <Text>Hello from React Native Web!</Text>
+    </View>
+  );
+};
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import App from './App.tsx';
+import { AppRegistry } from 'react-native';
+
+AppRegistry.registerComponent('App', () => App);
+
+AppRegistry.runApplication('App', {
+  initialProps: {},
+  rootTag: document.getElementById('root'),
+});
