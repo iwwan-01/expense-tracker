6f3c835657bafdb04f8c707e0ab2507a25ec2774
iwwan-01 <89984902+iwwan-01@users.noreply.github.com>
2024-01-27 20:18:59 +0200

    Added a stylesheet

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const App: React.FC = () => {
   return (
-    <View>
-      <Text>Hello from React Native Web!</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello from React Native Web!</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: 'black',
+  },
+});
 
 export default App;
